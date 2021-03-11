81b0c1f7abbd1fbbf58b9748aac1c2f37a2bcfc2
freust14 <freust14@gmail.com>
2021-03-11 12:42:36 -0600

    updated Employer and Skill classes

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,26 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    private String location;
+
+    public Employer(@NotBlank String location) {
+        this.location = location;
+    }
+
+    public Employer(){
+    }
+    public String getLocation() {
+        return location;
+    }
+    public void setLocation(String location) {
+        this.location = location;
+    }
 
 }
