7e44f395ebdf3c0bd2f9bdb123dd50e912ba0bae
freust14 <freust14@gmail.com>
2021-03-12 08:25:15 -0600

    set one-to-many relationship between Job, Employer classes

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,14 +1,21 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Employer extends AbstractEntity {
 
     @NotBlank
+    @NotNull
     private String location;
+
+    @OneToMany(mappedBy = "employer")
+    private final List<Job> jobs = new ArrayList<>();
 
     public Employer(@NotBlank String location) {
         this.location = location;
